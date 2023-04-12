3dae140bc9f818dbe1270045f9a1c5f349c2f856
Aleksei Danilenko <danilenko.aa@phystech.edu>
2023-04-13 00:47:15 +0300

    DE2-115: Update example

--- DE2-115/example/de2_115.v
+++ DE2-115/example/de2_115.v
@@ -13,5 +13,33 @@ module de2_115
   output wire [6:0]  HEX5,
   output wire [6:0]  HEX6
 );
-// Write code here
+wire [3:0] key_sync;
+button
+key0_sync
+(
+  .clk(CLOCK_50),
+  .button_async(KEY[0]),
+  .button_sync(key_sync[0])
+);
+button
+key1_sync
+(
+  .clk(CLOCK_50),
+  .button_async(KEY[1]),
+  .button_sync(key_sync[1])
+);
+button
+key2_sync
+(
+  .clk(CLOCK_50),
+  .button_async(KEY[2]),
+  .button_sync(key_sync[2])
+);
+button
+key3_sync
+(
+  .clk(CLOCK_50),
+  .button_async(KEY[3]),
+  .button_sync(key_sync[3])
+);
 endmodule
