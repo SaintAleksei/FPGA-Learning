fef3f8773776d3651bfd1eb740845ece2f2438c9
Aleksei Danilenko <danilenko.aa@phystech.edu>
2023-04-12 23:37:55 +0300

    DE2-115: Fix example

--- DE2-115/example/de2_115.v
+++ DE2-115/example/de2_115.v
@@ -1,16 +1,17 @@
 module de2_115
 (
-  input  wire        CLOCK_50,
-  input  wire [17:0] SW,
-  input  wire [3:0]  KEY,
-  output wire [17:0] LEDR,
-  output wire [8:0]  LEDG,
-  output wire [6:0]  HEX0,
+  input  wire        CLOCK_50, // Clock
+  input  wire [17:0] SW,       // Switches
+  input  wire [3:0]  KEY,      // Buttons, 1 when unpressed
+  output wire [17:0] LEDR,     // Red leds
+  output wire [8:0]  LEDG,     // Green leds
+  output wire [6:0]  HEX0,     // 7-segment displays
   output wire [6:0]  HEX1,
   output wire [6:0]  HEX2,
   output wire [6:0]  HEX3,
   output wire [6:0]  HEX4,
   output wire [6:0]  HEX5,
-  output wire [6:0]  HEX6,
+  output wire [6:0]  HEX6
 );
+// Write code here
 endmodule
