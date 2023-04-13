550eb7a061d96835afe5fadd43b78ecec22321ca
Danilenko Aleksei <danilenko.aa@phystech.edu>
2023-04-13 22:15:52 +0300

    DE2-115: shift_register: Update

--- DE2-115/shift_register/de2_115.v
+++ DE2-115/shift_register/de2_115.v
@@ -44,6 +44,15 @@ key3_sync
   .button_async(KEY[3]),
   .button_sync(key_sync[3])
 );
+reg [3:0] key_sync_repeat;
+always @(posedge CLOCK_50)
+begin
+  key_sync_repeat[0] <= key_sync[0];  
+  key_sync_repeat[1] <= key_sync[1];  
+  key_sync_repeat[2] <= key_sync[2];  
+  key_sync_repeat[3] <= key_sync[3];  
+end
+wire [3:0] key_sync_double = key_sync | key_sync_repeat;
 shiftreg
 #(
   .BIT_DEPTH(8)
@@ -53,9 +62,9 @@ sr
   .clk(CLOCK_50),
   .reset(key_sync[0]),
   .left_shift_bit(SW[0]),
-  .left_shift_event(key_sync[2]),
+  .left_shift_event(key_sync_double[2]),
   .right_shift_bit(SW[1]),
-  .right_shift_event(key_sync[1]),
+  .right_shift_event(key_sync_double[1]),
   .register(LEDR[7:0])
 ); 
 endmodule
