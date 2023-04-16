169ecb0cfc4bfb3f2e9c5855d6125d789e61c205
Aleksei Danilenko <danilenko.aa@phystech.edu>
2023-04-16 17:07:37 +0300

    DE2-115: tests: Make tests output look good

--- DE2-115/test/division.v
+++ DE2-115/test/division.v
@@ -3,106 +3,92 @@
 
 // (c) GPT-4
 module tb_division();
-    parameter BIT_DEPTH = 32;
-    reg clk;
-    reg reset;
-    reg start;
-    wire done;
-    reg [BIT_DEPTH-1:0] dividend;
-    reg [BIT_DEPTH-1:0] divisor;
-    wire [BIT_DEPTH-1:0] quotient;
-    wire [BIT_DEPTH-1:0] remainder;
+  parameter BIT_DEPTH = 32;
+  reg clk;
+  reg reset;
+  reg start;
+  wire done;
+  reg [BIT_DEPTH-1:0] dividend;
+  reg [BIT_DEPTH-1:0] divisor;
+  wire [BIT_DEPTH-1:0] quotient;
+  wire [BIT_DEPTH-1:0] remainder;
 
-    // Instantiate the division module
-    division #(BIT_DEPTH) uut (
-        .clk(clk),
-        .reset(reset),
-        .start(start),
-        .done(done),
-        .dividend_in(dividend),
-        .divisor_in(divisor),
-        .quotient(quotient),
-        .remainder(remainder)
-    );
+  // Instantiate the division module
+  division #(BIT_DEPTH) uut (
+    .clk(clk),
+    .reset(reset),
+    .start(start),
+    .done(done),
+    .dividend_in(dividend),
+    .divisor_in(divisor),
+    .quotient(quotient),
+    .remainder(remainder)
+  );
 
-    // Clock generation
-    always begin
-        #5 clk = ~clk;
+  // Clock generation
+  always begin
+    #5 clk = ~clk;
+  end
+
+  // Check result task
+  reg [7:0] test_case_number = 0;
+  task run_test_case;
+    input [BIT_DEPTH-1:0] dividend_in;
+    input [BIT_DEPTH-1:0] divisor_in;
+    input [BIT_DEPTH-1:0] exp_quotient;
+    input [BIT_DEPTH-1:0] exp_remainder;
+    begin
+      test_case_number = test_case_number + 1;
+      $display("Test case #%d", test_case_number);
+
+      dividend = dividend_in;
+      divisor  = divisor_in;
+      start    = 1;
+      @(posedge done);
+      #10;
+      start    = 0;
+      #10;
+      if (quotient === exp_quotient && remainder === exp_remainder) begin
+        $display("\tTest passed: %d / %d = %d, remainder = %d", dividend, divisor, quotient, remainder);
+      end else begin
+        $display("\tTest failed: %d / %d", dividend, divisor);
+        $display("\t  Expected: quotient = %d, remainder = %d", exp_quotient, exp_remainder);
+        $display("\t  Got: quotient = %d, remainder = %d", quotient, remainder);
+        $finish;
+      end
     end
+  endtask
 
-    // Check result task
-    task check_result;
-        input [BIT_DEPTH-1:0] exp_quotient;
-        input [BIT_DEPTH-1:0] exp_remainder;
-        begin
-            if (quotient === exp_quotient && remainder === exp_remainder) begin
-                $display("\tTest passed: %d / %d = %d, remainder = %d", dividend, divisor, quotient, remainder);
-            end else begin
-                $display("\tTest failed: %d / %d", dividend, divisor);
-                $display("\t  Expected: quotient = %d, remainder = %d", exp_quotient, exp_remainder);
-                $display("\t  Got: quotient = %d, remainder = %d", quotient, remainder);
-                $finish;
-            end
-        end
-    endtask
+  // Testbench stimulus
+  initial begin
+    $dumpfile("tb_division.vcd");
+    $dumpvars(0, tb_division);
+    $display("Running tests for module \"division\"...");
+    clk = 0;
+    reset = 1;
+    start = 0;
+    dividend = 0;
+    divisor = 0;
+    #10 reset = 0;
 
-    // Testbench stimulus
-    initial begin
-        $dumpfile("testbench.vcd");
-        $dumpvars(0, tb_division);
-        $display("Running tests for module \"division\"...");
-        clk = 0;
-        reset = 1;
-        start = 0;
-        dividend = 0;
-        divisor = 0;
-        #10 reset = 0;
+    // Test case 1: 100 / 4
+    run_test_case(100, 4, 25, 0);
+    
+    // Test case 2: 256 / 16
+    run_test_case(256, 16, 16, 0);
+    
+    // Test case 3: 1234 / 56
+    run_test_case(1234, 56, 22, 2);
 
-        // Test case 1: 100 / 4
-        start = 1;
-        dividend = 100;
-        divisor = 4;
-        @(posedge done);
-        #10 check_result(25, 0);
-        start = 0;
-        
-        // Test case 2: 256 / 16
-        #10 start = 1;
-        dividend = 256;
-        divisor = 16;
-        @(posedge done);
-        #10 check_result(16, 0);
-        start = 0;
-        
-        // Test case 3: 1234 / 56
-        #10 start = 1;
-        dividend = 1234;
-        divisor = 56;
-        @(posedge done);
-        #10 check_result(22, 2);
-        start = 0;
+    // Test case 4: 65535 / 255
+    run_test_case(65535, 255, 257, 0);
 
-        // Test case 4: 65535 / 255
-        #10 start = 1;
-        dividend = 65535;
-        divisor = 255;
-        @(posedge done);
-        #10 check_result(257, 0);
-        start = 0;
+    // Test case 5: 100 / 0
+    run_test_case(100, 0, 0, 0);
 
-        // Test case 5: 100 / 0
-        #10 start = 1;
-        dividend = 100;
-        divisor = 0;
-        @(posedge done);
-        // In this case, the result check will depend on the behavior of your division module when dividing by zero.
-        // Assuming that the quotient and remainder will be undefined zeros, use the following check:
-        #10 check_result({BIT_DEPTH{1'b0}}, {BIT_DEPTH{1'b0}});
-        start = 0;
-
-        // Finish the simulation
-        $display("All tests are passed!");
-        #10 $finish;
-    end
+    // Finish the simulation
+    $display("All tests are passed!");
+    #10 $finish;
+  end
 endmodule
 
--- DE2-115/test/notation.v
+++ DE2-115/test/notation.v
@@ -1,15 +1,16 @@
 `include "library.v"
 `timescale 1ns / 1ps
 
+// (c) GPT-4
 module tb_notation();
   // Parameters for different instances
-  localparam BIT_DEPTH1 = 8;
-  localparam NUM_DIGITS1 = 3;
-  localparam BASE1 = 10;
+  parameter BIT_DEPTH1 = 8;
+  parameter NUM_DIGITS1 = 3;
+  parameter BASE1 = 10;
 
-  localparam BIT_DEPTH2 = 4;
-  localparam NUM_DIGITS2 = 4;
-  localparam BASE2 = 2;
+  parameter BIT_DEPTH2 = 4;
+  parameter NUM_DIGITS2 = 4;
+  parameter BASE2 = 2;
 
   // Test signals
   reg clk;
@@ -45,16 +46,16 @@ module tb_notation();
   end
 
   // Test case execution task
-  integer test_case_number = 0;
+  reg [7:0] test_case_number = 0;
   task run_test_case;
     input [BIT_DEPTH1-1:0] in_num1;
-    input [(NUM_DIGITS1 * BIT_DEPTH1)-1:0] exp_out1;
     input [BIT_DEPTH2-1:0] in_num2;
+    input [(NUM_DIGITS1 * BIT_DEPTH1)-1:0] exp_out1;
     input [(NUM_DIGITS2 * BIT_DEPTH2)-1:0] exp_out2;
 
     begin
       test_case_number = test_case_number + 1;
-      $display("Test case %0d", test_case_number);
+      $display("Test case #%d:", test_case_number);
       
       // Set input numbers
       number1 = in_num1;
@@ -71,16 +72,12 @@ module tb_notation();
       end
 
       // Check results and display test case status
-      if (digits1 == exp_out1 && conversion_done1) begin
-        $display("\tInstance 1: PASSED | Input: %0d | Expected Output: %0h | Actual Output: %0h", in_num1, exp_out1, digits1);
+      if (digits1 == exp_out1 && digits2 == exp_out2) begin
+        $display("\tTest passed: 8'd%d = %h, 8'b%b = %h", in_num1, digits1, in_num2, digits2);
       end else begin
-        $display("\tInstance 1: FAILED | Input: %0d | Expected Output: %0h | Actual Output: %0h", in_num1, exp_out1, digits1);
-      end
-
-      if (digits2 == exp_out2 && conversion_done2) begin
-        $display("\tInstance 2: PASSED | Input: %0b | Expected Output: %0b | Actual Output: %0b", in_num2, exp_out2, digits2);
-      end else begin
-        $display("\tInstance 2: FAILED | Input: %0b | Expected Output: %0b | Actual Output: %0b", in_num2, exp_out2, digits2);
+        $display("\tTest failed: 8'd%d = %h, 8'b%b = %h", in_num1, digits1, in_num2, digits2);
+        $display("\tExpected:    8'd%d = %h, 8'b%b = %h", in_num1, exp_out1, in_num2, exp_out2);
+        $finish;
       end
     end
   endtask
@@ -89,21 +86,22 @@ module tb_notation();
   initial begin
     $dumpfile("tb_notation.vcd");
     $dumpvars(0, tb_notation);
-    $display("Running tests for notation module");
+    $display("Running tests for \"notation\" module");
     // Initialize signals
     clk = 0;
     number1 = 0;
     number2 = 0;
 
     // Test case 1
-    run_test_case(8'd123, {8'd1, 8'd2, 8'd3}, 4'b1010, {4'b1, 4'b0, 4'b1, 4'b0});
+    run_test_case(8'd123, 4'b1010, {8'd1, 8'd2, 8'd3}, {4'b1, 4'b0, 4'b1, 4'b0});
 
     // Test case 2
-    run_test_case(8'd250, {8'd2, 8'd5, 8'd0}, 4'b0110, {4'b0, 4'b1, 4'b1, 4'b0});
+    run_test_case(8'd250, 4'b0110, {8'd2, 8'd5, 8'd0}, {4'b0, 4'b1, 4'b1, 4'b0});
 
     // Add more test cases below by calling run_test_case() with appropriate arguments
     // ...
 
+    $display("All tests are passed!");
     // Finish the simulation
     $finish;
   end
