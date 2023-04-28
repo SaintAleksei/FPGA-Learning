cdb4abb92606740fc645ef493479ee0619d0efa5
Danilenko Aleksei <danilenko.aa@phystech.edu>
2023-04-28 14:32:26 +0300

    library: add vga_text module for working with VGA text graphics

--- library.v
+++ library.v
@@ -1,3 +1,123 @@
+module vga_text
+#(
+  // VGA timings 
+  parameter HOR_FRONT_PORCH = 16,
+  parameter HOR_SYNC_PULSE  = 96,
+  parameter HOR_BACK_PORCH  = 48,
+  parameter HOR_RES         = 640,
+  parameter VER_FRONT_PORCH = 10,
+  parameter VER_SYNC_PULSE  = 2,
+  parameter VER_BACK_PORCH  = 33,
+  parameter VER_RES         = 480,
+
+  parameter COLOR_BIT_DEPTH = 8,
+  parameter HOR_BIT_DEPTH   = 12,
+  parameter VER_BIT_DEPTH   = 12,
+
+  parameter FRONT_WIDTH      = 16,
+  parameter FRONT_HEIGH      = 8,
+
+  parameter TEXT_SYMS_PER_LINE    = HOR_RES / FRONT_WIDTH,
+  parameter TEXT_LINES_PER_SCREEN = VER_RES / FRONT_HEIGH
+)
+(
+  // Clock and reset
+  input  wire clk,
+  input  wire reset,
+
+  // Text buffer
+  input  wire [TEXT_SYMS_PER_LINE * TEXT_LINES_PER_SCREEN * 8 - 1:0] text,
+  
+  // This wires should be connected to font module
+  input  wire sym_pixel,
+  output wire [7:0] sym_code,
+  output wire [HOR_BIT_DEPTH-1:0] sym_x,
+  output wire [VER_BIT_DEPTH-1:0] sym_y,
+
+  // This wire should be connected to VGA (through COLOT_BIT_DEPTH-bit DAC)
+  output wire [COLOR_BIT_DEPTH-1:0] red,
+  output wire [COLOR_BIT_DEPTH-1:0] green,
+  output wire [COLOR_BIT_DEPTH-1:0] blue,
+  output wire hsync,
+  output wire vsync,
+  output wire sync_n,
+  output wire blank_n
+);
+  localparam HOR_BACK_PORCH_START = HOR_SYNC_PULSE;
+  localparam HOR_DATA_START = HOR_BACK_PORCH_START + HOR_BACK_PORCH;
+  localparam HOR_FRONT_PORCH_START = HOR_DATA_START + HOR_RES;
+  localparam VER_BACK_PORCH_START = VER_SYNC_PULSE;
+  localparam VER_DATA_START = VER_BACK_PORCH_START + VER_BACK_PORCH;
+  localparam VER_FRONT_PORCH_START = VER_DATA_START + VER_RES;
+
+  reg [HOR_BIT_DEPTH-1:0] hcnt;
+  reg [VER_BIT_DEPTH-1:0] vcnt;
+
+  assign sync_n = 1'b0;
+  assign blank_n = (hcnt >= HOR_DATA_START) &
+                   (hcnt < HOR_FRONT_PORCH_START) &
+                   (vcnt >= VER_DATA_START) &
+                   (vcnt < VER_FRONT_PORCH_START);
+  assign hsync = (hcnt >= HOR_BACK_PORCH_START);
+  assign vsync = (vcnt >= VER_BACK_PORCH_START);
+  assign show_pixel = (hcnt >= HOR_DATA_START && hcnt < HOR_FRONT_PORCH) &
+                      (vcnt >= VER_DATA_START && vcnt < VER_FRONT_PORCH); 
+  assign red   = {COLOR_BIT_DEPTH{show_pixel}};
+  assign green = red;
+  assign blue  = red;
+
+  wire [HOR_BIT_DEPTH-1:0] hor_sym_cnt;
+  wire [HOR_BIT_DEPTH-1:0] ver_sym_cnt;
+  division_flash
+  #(
+    .BIT_DEPTH(HOR_BIT_DEPTH)
+  )
+  hcnt_div
+  (
+    .dividend(hcnt),
+    .divisor(FRONT_WIDTH),
+    .quotient(hor_sym_cnt),
+    .remainder(sym_x)
+  );
+
+  division_flash
+  #(
+    .BIT_DEPTH(VER_BIT_DEPTH) 
+  )
+  vcnt_div
+  (
+    .dividend(vcnt),
+    .divisor(FRONT_HEIGH),
+    .quotient(ver_sym_cnt),
+    .remainder(sym_y)
+  );
+
+  wire [7:0] text_array [TEXT_SYMS_PER_LINE * TEXT_LINES_PER_SCREEN - 1:0];
+  genvar gi;
+  generate 
+    for (gi = 0; gi < TEXT_SYMS_PER_LINE * TEXT_LINES_PER_SCREEN; gi = gi + 1)
+    begin: text_array_loop
+      assign text_array[gi] = text[(gi + 1) * 8 - 1: gi * 8];
+    end
+  endgenerate
+  
+
+  assign sym_code = text_array[hor_sym_cnt + ver_sym_cnt * TEXT_SYMS_PER_LINE];
+  
+  always @(posedge clk)
+    if (reset)
+    begin
+      hcnt <= 0;
+      vcnt <= 0;
+    end
+    else
+    begin
+      hcnt <= (hcnt == HOR_RES - 1) ? 0 : hcnt + 1;
+      vcnt <= (hcnt != HOR_RES - 1) ? vcnt :
+              (vcnt == VER_RES - 1) ? 0 : vcnt + 1;
+  end
+endmodule 
+
 /*
  *                Memory module
  *  One-tact `write` pulse need to write `val2write` 
